07febbe2bf0347adfc39d687c2848f5e171cd50a
devbuildgc <dev@buildgc.com>
2022-11-17 20:38:43 -0800

    First

---
+++ QABBB/API/Models/User/NewUserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace QABBB.API.Models.User
+{
+    public class UserDTO
+    {
+        public int IdUser { get; set; }
+
+        public string UserName { get; set; }
+
+        public bool? IsDarkMode { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
+
