876b7db0370e899cf8d40b4e9d08d5ff567c6a5c
devbuildgc <zabele@bara.com.br>
2023-01-06 18:24:02 -0800

    Swagger

--- QABBB/Program.cs
+++ QABBB/Program.cs
@@ -77,11 +77,11 @@ app.UseForwardedHeaders(new ForwardedHeadersOptions
 });
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 //app.UseHttpsRedirection();
 
